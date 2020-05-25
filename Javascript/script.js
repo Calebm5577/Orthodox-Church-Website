718452d6d39eb7f5b5b1314a1dea4bb315058e7a
Calebm5577 <calebm5577@gmail.com>
2020-05-25 16:26:02 -0400

    power-out commit

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,28 @@
 
-//switch text and description for saints onclick
+//Switch saint name and description onclick
 
-function highlight() {
-    var img = document.querySelector("img")
-    img.style.height = 200;
-    img.style.width = 200;
+const mainText = document.querySelector("h1.main-textbox-about-description-text");
+const secondaryText = document.querySelector("h1.main-textbox-about-description-text-long")
+const imgContainer = document.querySelector('.main-textbox-about-saints');
+
+imgContainer.addEventListener('click', (e) => {
+
+
+  mainText.textContent = e.target.dataset.caption;
+
+});
+
+
+function Saintdesc(text) {
+  var el1 = document.querySelector(".main-textbox-about-description-text");
+  var el2 = document.querySelector(".main-textbox-about-description-long-text");
+  el1.textContent = text;
+  el2.textContent = text;
 }
+
+
+
+
+
+
+
