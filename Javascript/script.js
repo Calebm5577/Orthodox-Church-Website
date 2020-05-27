06537b8cd42387a231ce2742e69a9859de3f5c82
Calebm5577 <calebm5577@gmail.com>
2020-05-27 14:33:02 -0400

    5/27 commit

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,9 @@
 
 //Switch saint name and description onclick
 
-const mainText = document.querySelector("h1.main-textbox-about-description-text");
-const secondaryText = document.querySelector("h1.main-textbox-about-description-text-long")
-const imgContainer = document.querySelector('.main-textbox-about-saints');
+const mainText = document.querySelector("h1.main-textbox-ourfaith-description-text");
+const secondaryText = document.querySelector("h1.main-textbox-ourfaith-description-text-long")
+const imgContainer = document.querySelector('.main-textbox-ourfaith-saints');
 
 imgContainer.addEventListener('click', (e) => {
 
@@ -14,8 +14,8 @@ imgContainer.addEventListener('click', (e) => {
 
 
 function Saintdesc(text) {
-  var el1 = document.querySelector(".main-textbox-about-description-text");
-  var el2 = document.querySelector(".main-textbox-about-description-long-text");
+  var el1 = document.querySelector(".main-textbox-ourfaith-description-text");
+  var el2 = document.querySelector(".main-textbox-ourfaith-description-long-text");
   el1.textContent = text;
   el2.textContent = text;
 }
